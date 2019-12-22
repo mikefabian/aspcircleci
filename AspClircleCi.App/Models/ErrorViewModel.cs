32adc087ecbf5c7828d3a68fbf4537cb2ef3fd5a
Mike Coney Fabián <mike.fabian@labcode.com.do>
2019-12-22 13:26:14 -0400

    config

---
+++ AspClircleCi.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspClircleCi.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
